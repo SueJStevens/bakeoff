4d9f4c1230cddd22c0ee9f0e8aa8b28ee42bba04
SueJStevens <StevensStock@gmail.com>
2018-11-12 19:50:33 -0800

    mods to connection for heroku jaws sql

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Bakeoff
 Bootcamp Assignment 11/10/2018 - Node Express Handlebars MVC Design (Burger)
+
+https://blooming-depths-36189.herokuapp.com/ 
--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
+//add code to read and set any environment variables with the dotenv package:
+require("dotenv").config();
+
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Su3J3ns3n",
-  database: "bakeoff_db"
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+  
+  });
+}
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
